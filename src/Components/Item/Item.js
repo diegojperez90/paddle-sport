6dcb01929612ef8f10808afa0efed22beadfa131
Diego J Perez <diegojperez90@gmail.com>
2022-06-20 09:38:59 -0300

    funcionalidad: agrego item detail container con su respectivo item detail

--- src/Components/Item/Item.js
+++ src/Components/Item/Item.js
@@ -1,10 +1,13 @@
-export default function Item({title, category, imagen}) {
+import { productos } from '../../Products'
+import './Item.css'
+
+export default function Item({title, category, imagen, id}) {
 
   return (
-    <div >
+    <div className="cajaItem">
       <h3>{ title }</h3>
       <h4>{ category }</h4>
-      <img  src = {imagen} width="200"/>
+      <img  src = {imagen} alt={title} width="200"/>
     </div>
   )
 }
--- src/Components/ItemList/ItemList.js
+++ src/Components/ItemList/ItemList.js
@@ -1,8 +1,9 @@
 import Item from "../Item/Item";
+import './ItemList.css'
 
 export default function ItemList({ items }) {
   return (
-    <div>
+    <div className="item">
       { items.map((prod)=>(
         <Item key={prod.id} title = {prod.title} category = {prod.category} imagen = {prod.imagen}/>
       ))}
