61f691298f780bd1c700b6425b25eb42dbfd5f5f
Diego J Perez <diegojperez90@gmail.com>
2022-07-05 01:54:26 -0300

    funcionalidad: agrego detalles al context

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -6,8 +6,22 @@ export function CartContextProvider(props) {
   const [cart, setCart] = useState([])
 
   function addItem (item, cantidad) {
-    let productosAgregados = [...cart, {...item, cantidad}];
-    setCart(productosAgregados)
+    if (isInCartContext(item.id)){
+      let itemToAdd = cart.find( i => i.id === item.id);
+      itemToAdd.cantidad += cantidad;
+
+      let newCart =  cart.filter( i => i.id !== item.id);
+      setCart([...newCart, {...itemToAdd}]);
+
+    } else {
+      let productosAgregados = [...cart, {...item, cantidad}];
+      setCart(productosAgregados)
+    }
+  }
+
+  function removerItem(id){
+    let newCart =  cart.filter( item => item.id !== id);
+    setCart(newCart)
   }
 
   function isInCartContext(id){
@@ -20,12 +34,18 @@ export function CartContextProvider(props) {
     return total;
   }
 
+  function totalPrecioCart(){
+    let total = 0;
+    cart.forEach( item => total = total + (item.cantidad * item.price));
+    return total;
+  }
+
   function clearCart() {
     setCart([]);
 };
 
   return (
-    <CartContext.Provider value={ { cart, addItem, isInCartContext, qntyInCart ,clearCart } }>
+    <CartContext.Provider value={ { cart, addItem, isInCartContext, qntyInCart , totalPrecioCart ,clearCart, removerItem } }>
       {props.children}
     </CartContext.Provider>  
   )
